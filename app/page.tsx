f10beb0b43726154eaca5c45a6ac106eebcd5283
Dharvipatel10 <dharvipatel1610@gmail.com>
2026-06-29 11:29:57 +0200

    Implement a EventDetail page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import ExploreBtn from "./components/ExploreBtn";
-import EventCard from "./components/EventCard";
-import events from "../lib/constants";
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
